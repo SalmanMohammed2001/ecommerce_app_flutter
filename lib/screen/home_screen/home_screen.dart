fb69443715a30a9a2388ff9ff20fcdd4b33e9769
salman <salmannisthar123@gmail.com>
2024-07-21 23:14:27 +0530

    custom_icon_button done

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecommerce_app/data/demo_data.dart';
 import 'package:ecommerce_app/screen/home_screen/widget/categories.dart';
 import 'package:ecommerce_app/screen/home_screen/widget/custom_app_bar.dart';
+import 'package:ecommerce_app/screen/home_screen/widget/custom_icon_button.dart';
 import 'package:ecommerce_app/screen/home_screen/widget/news_slider.dart';
 import 'package:ecommerce_app/screen/home_screen/widget/product_grid.dart';
 import 'package:flutter/material.dart';
@@ -32,6 +33,48 @@ class _HomeScreenState extends State<HomeScreen> {
                   height: 8,
                 ),
                 NewsSlider(size: size),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black87,
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        CustomIconButton(
+                          icon: Icons.people,
+                          text: "My Profile",
+                          color: Colors.green,
+                          bgColor: Colors.green.shade100,
+                        ),
+                        CustomIconButton(
+                          icon: Icons.favorite,
+                          text: " Favorite",
+                          color: Colors.red,
+                          bgColor: Colors.red.shade100,
+                        ),
+                        CustomIconButton(
+                          icon: Icons.shopping_cart_rounded,
+                          text: " My Orders",
+                          color: Colors.blue,
+                          bgColor: Colors.blue.shade100,
+                        ),
+                        CustomIconButton(
+                          icon: Icons.web,
+                          text: " Web",
+                          color: Colors.brown,
+                          bgColor: Colors.brown.shade100,
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
                 const Categories(),
                 const ProductGrid()
               ],
@@ -42,7 +85,6 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
 
 /*
 * Card(
