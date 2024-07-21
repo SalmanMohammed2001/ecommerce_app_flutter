f76aa9499d31b89585d488319bac94d75b803aaf
salman <salmannisthar123@gmail.com>
2024-07-21 22:50:03 +0530

    product grid widget create

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:ecommerce_app/data/demo_data.dart';
 import 'package:ecommerce_app/screen/home_screen/widget/categories.dart';
 import 'package:ecommerce_app/screen/home_screen/widget/custom_app_bar.dart';
 import 'package:ecommerce_app/screen/home_screen/widget/news_slider.dart';
+import 'package:ecommerce_app/screen/home_screen/widget/product_grid.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -32,71 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 NewsSlider(size: size),
                 const Categories(),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  "The latest",
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
-                ),
-                const Text(
-                  "Take a look at what’s new, right now.",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
-                ),
-
-                GridView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: DemoData.products.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                            childAspectRatio: 0.99,
-                            crossAxisSpacing: 15,
-                            mainAxisSpacing: 5),
-                    itemBuilder: (context, index) => SizedBox(
-                          height: 200,
-                          child: Stack(
-                            children: [
-                              Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Container(
-                                  height: 100,
-                                  decoration: BoxDecoration(
-                                      color: Colors.black87,
-                                      borderRadius: BorderRadius.circular(25)),
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.center,
-                                child: Image.network(
-                                  DemoData.products[index].image,
-                                  height: 100,
-                                  fit: BoxFit.fitHeight,
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      DemoData.products[index].name,
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    Text(
-                                      '\$ ${DemoData.products[index].price}',
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                    const SizedBox(
-                                      height: 5,
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ))
+                const ProductGrid()
               ],
             ),
           ),
@@ -105,6 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
 
 /*
 * Card(
---
+++ lib/screen/home_screen/widget/product_grid.dart
@@ -0,0 +1,86 @@
+
+import 'package:flutter/material.dart';
+
+import '../../../data/demo_data.dart';
+
+class ProductGrid extends StatelessWidget {
+  const ProductGrid({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        const Text(
+          "The latest",
+          style:
+          TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
+        ),
+        const Text(
+          "Take a look at what’s new, right now.",
+          style:
+          TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+        ),
+        GridView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemCount: DemoData.products.length,
+            gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.99,
+                crossAxisSpacing: 15,
+                mainAxisSpacing: 5),
+            itemBuilder: (context, index) => SizedBox(
+              height: 200,
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      height: 100,
+                      decoration: BoxDecoration(
+                          color: Colors.black87,
+                          borderRadius:
+                          BorderRadius.circular(25)),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Image.network(
+                      DemoData.products[index].image,
+                      height: 100,
+                      fit: BoxFit.fitHeight,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          DemoData.products[index].name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          '\$ ${DemoData.products[index].price}',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ))
+      ],
+    );
+  }
+}
