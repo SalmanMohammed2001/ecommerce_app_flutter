1204435a97c8c3a0f5599fe286bff9e6093f0112
salman <salmannisthar123@gmail.com>
2024-07-21 12:34:34 +0530

    create CategoryModel

--- lib/screen/home_screen/home_screen.dart
+++ lib/screen/home_screen/home_screen.dart
@@ -14,19 +14,35 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
 
-    final size= MediaQuery.sizeOf(context);
-
-    return  SafeArea(
+    return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const CustomAppBar(),
-              const SizedBox(height: 8,),
-              NewsSlider(size: size)
+              const SizedBox(
+                height: 8,
+              ),
+              NewsSlider(size: size),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Apple Store",
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
+              ),
+              const Text(
+                "The best way to buy the products you love.",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
             ],
           ),
         ),
